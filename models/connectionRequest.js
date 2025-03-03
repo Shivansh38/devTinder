d80a1dd3787ed3e7333585faa1dc06ceb2101c54
Shivansh <shivanshdev10@gmail.com>
2025-03-03 22:39:38 +0530

    added connection request

--- models/connectionRequest.js
+++ models/connectionRequest.js
@@ -2,16 +2,29 @@ const mongoose = require('mongoose');
 
 const connectionRequestSchema = new mongoose.Schema({
     fromUserId:{
-        type: mongoose.Schema.Types.ObjectId
+        type: mongoose.Schema.Types.ObjectId,
+        required:true
     },
     toUserId:{
-        type: mongoose.Schema.Types.ObjectId
+        type: mongoose.Schema.Types.ObjectId,
+        required:true
     },
     status:{
         type:'string',
+        required:true,
         status: {
-            value:["ignore","intersted","pending","rejected"]
+            value:["ignore","intersted","pending","rejected"],
+            message: `{VALUE} is incorrect status type`
         }
     },
 
-})
+},{
+    timestamps:true,
+});
+
+const connectionRequestModel = new mongoose.model(
+    "connectionRequest",
+    connectionRequestSchema
+)
+
+module.exports = connectionRequestModel;
